915a93e5d419ece696d3ecc49c217576fd628bbe
harsh-codess <harshgidwani2007@gmail.com>
2026-07-14 00:31:00 +0530

    fuck dsa

--- c/dsa-lab/sparseaddition.c
+++ c/dsa-lab/sparseaddition.c
@@ -4,6 +4,33 @@
 
 #define MAX 10
 
-void accept_matrix(int a[MAX][MAX], ) { 
+void accept_matrix(int a[MAX][MAX], int rows, int cols) { 
+    for(int i = 0; i < rows; i++) { 
+        for(int j = 0; j < cols; j++ ) { 
+            printf("Enter matrix (%d x %d):\n", rows, cols);
+            scanf("%d", &a[i][j]);
+        }
+
+    }
+
+
+void compart_matrix()  { 
+    int sparsecounter = 1; 
+
+
+    for(int i = 0; i<rows; i++) { 
+        for (int j = 0; j<coloumns; j++) { 
+            if([a[i][j] != 0 ) { 
+                [k][0] = i; 
+                
+
+
+            }
+
+        }
+    }
+}
+
+
 
 }
